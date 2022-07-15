18672e4ff57d60a347b5ea9a4f2976081f747776
Robin Kalleicher <robin.kalleicherqaware.de>
2022-07-15 11:46:10 +0200

    implement vue frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Cloudy logo" src="./assets/cloudy.png" height="300">
+  <SpringWeather title="Welcome to this Spring Boot Weather Application" msg=""/>
+</template>
+
+<script>
+import SpringWeather from './components/SpringWeather.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SpringWeather
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/SpringWeather.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+  </div>
+  <p>{{msg}}</p>
+  <div>
+    <input v-model="cityInput" placeholder="Where do you live?" />
+    <button @click="search">How is the weather?</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SpringWeather',
+  props: {
+    title: String
+  },
+  data() {
+    return {
+      msg: "Hier könnte Ihre Werbung stehen",
+      cityInput: ""
+    }
+  },
+  methods: {
+    search() {
+      fetch("/api/weather?city=" + this.cityInput)
+          .then((response) => response.text())
+          .then((data) => {this.msg = data;});
+    }
+  }
+}
+
+</script>
+
+<!-- "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+input {
+  margin-right: 4px;
+}
+</style>
