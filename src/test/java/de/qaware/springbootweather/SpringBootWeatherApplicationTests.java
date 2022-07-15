cdeda927d236a5a7c5a67b87626564ced83cdc12
Robin Kalleicher <robin.kalleicherqaware.de>
2022-07-15 11:53:27 +0200

    fix test cases

--- src/test/java/de/qaware/springbootweather/SpringBootWeatherApplicationTests.java
+++ src/test/java/de/qaware/springbootweather/SpringBootWeatherApplicationTests.java
@@ -22,36 +22,28 @@ class SpringBootWeatherApplicationTests {
     }
 
     @Test
-    void shouldRetrieveCorrectWeatherDEFAULT() {
-        String output = trt.getForObject("http://localhost:" + port + "/weather", String.class);
-
-        // Assertions:
-        assertThat(output).contains("The weather in 'Mainz' is currently: 'Sunshine'. Last update:");
-    }
-
-    @Test
     void shouldRetrieveCorrectWeatherRosenheim() {
-        String output = trt.getForObject("http://localhost:" + port + "/weather?city=Rosenheim", String.class);
+        String output = trt.getForObject("http://localhost:" + port + "/api/weather?city=Rosenheim", String.class);
 
         // Assertions:
-        assertThat(output).contains("The weather in 'Rosenheim' is currently: 'Mega Sunshine'. Last update:");
+        assertThat(output).contains("The weather in 'Rosenheim' is currently: ");
     }
 
     @Test
     void shouldRetrieveCorrectWeatherMunich() {
-        String output = trt.getForObject("http://localhost:" + port + "/weather?city=Munich", String.class);
+        String output = trt.getForObject("http://localhost:" + port + "/api/weather?city=Munich", String.class);
 
         // Assertions:
-        assertThat(output).contains("The weather in 'Munich' is currently: 'Sunny sunny sunny'. Last update:");
+        assertThat(output).contains("The weather in 'Munich' is currently: ");
     }
 
     @Test
     void shouldAddToDataBaseSuccessfully() {
         // Add data to the database
-        String id = trt.postForObject("http://localhost:" + port + "/add?city=Test&weather=TestWeather", null, String.class);
+        String id = trt.postForObject("http://localhost:" + port + "/api/add?city=Test&weather=TestWeather", null, String.class);
 
         // Retrieve data
-        String output2 = trt.getForObject("http://localhost:" + port + "/weather?city=Test", String.class);
+        String output2 = trt.getForObject("http://localhost:" + port + "/api/weather?city=Test", String.class);
 
         // Assertions:
         assertThat(output2).contains("The weather in 'Test' is currently: 'TestWeather'. Last update:");
@@ -63,10 +55,10 @@ class SpringBootWeatherApplicationTests {
     @Test
     void shouldDeleteFromDataBaseSuccessfully() {
         // Add data to the database
-        String id = trt.postForObject("http://localhost:" + port + "/add?city=Test&weather=TestWeather", null, String.class);
+        String id = trt.postForObject("http://localhost:" + port + "/api/add?city=Test&weather=TestWeather", null, String.class);
 
         // Delete created data:
-        String output = trt.postForObject("http://localhost:" + port + "/delete?id=" + id, null, String.class);
+        String output = trt.postForObject("http://localhost:" + port + "/api/delete?id=" + id, null, String.class);
 
         // Assertions:
         assertThat(output).isEqualTo("Deleted successfully");
@@ -75,31 +67,31 @@ class SpringBootWeatherApplicationTests {
     @Test
     void shouldAddAndDeleteFromDataBaseCorrectly() {
         // Add data to the database
-        String id = trt.postForObject("http://localhost:" + port + "/add?city=Test&weather=TestWeather", null, String.class);
+        String id = trt.postForObject("http://localhost:" + port + "/api/add?city=DeletionTest&weather=TestWeather", null, String.class);
 
         // Retrieve data
-        String output = trt.getForObject("http://localhost:" + port + "/weather?city=Test", String.class);
+        String output = trt.getForObject("http://localhost:" + port + "/api/weather?city=DeletionTest", String.class);
 
         // Delete created data:
-        String output2 = trt.postForObject("http://localhost:" + port + "/delete?id=" + id, null, String.class);
+        String output2 = trt.postForObject("http://localhost:" + port + "/api/delete?id=" + id, null, String.class);
 
         // Retrieve again
-        String output3 = trt.getForObject("http://localhost:" + port + "/weather?city=Test", String.class);
+        String output3 = trt.getForObject("http://localhost:" + port + "/api/weather?city=DeletionTest", String.class);
 
         // Assertions:
-        assertThat(output).contains("The weather in 'Test' is currently: 'TestWeather'. Last update:");
+        assertThat(output).contains("The weather in 'DeletionTest' is currently: ");
         assertThat(output2).isEqualTo("Deleted successfully");
-        assertThat(output3).doesNotContain("The weather in 'London' is currently: 'Rainy'. Last update:");
+        assertThat(output3).doesNotContain("The weather in 'DeletionTest' is currently: ");
     }
 
     @Test
     void shouldUseLatestValueFromDataBase() {
         // Add data to the database
-        String id1 = trt.postForObject("http://localhost:" + port + "/add?city=Test&weather=TestWeather1", null, String.class);
-        String id2 = trt.postForObject("http://localhost:" + port + "/add?city=Test&weather=TestWeather2", null, String.class);
+        String id1 = trt.postForObject("http://localhost:" + port + "/api/add?city=Test&weather=TestWeather1", null, String.class);
+        String id2 = trt.postForObject("http://localhost:" + port + "/api/add?city=Test&weather=TestWeather2", null, String.class);
 
         // Retrieve data
-        String output = trt.getForObject("http://localhost:" + port + "/weather?city=Test", String.class);
+        String output = trt.getForObject("http://localhost:" + port + "/api/weather?city=Test", String.class);
 
         // Assertions:
         assertThat(output).contains("The weather in 'Test' is currently: 'TestWeather2'. Last update:");
@@ -112,12 +104,12 @@ class SpringBootWeatherApplicationTests {
     @Test
     void shouldListWeatherFromDataBaseSuccessfully() {
         // prepare data
-        String id1 = trt.postForObject("http://localhost:" + port + "/add?city=Test1&weather=Rainy", null, String.class);
-        String id2 = trt.postForObject("http://localhost:" + port + "/add?city=Test2&weather=Hot", null, String.class);
-        String id3 = trt.postForObject("http://localhost:" + port + "/add?city=Test3&weather=Very Hot", null, String.class);
+        String id1 = trt.postForObject("http://localhost:" + port + "/api/add?city=Test1&weather=Rainy", null, String.class);
+        String id2 = trt.postForObject("http://localhost:" + port + "/api/add?city=Test2&weather=Hot", null, String.class);
+        String id3 = trt.postForObject("http://localhost:" + port + "/api/add?city=Test3&weather=Very Hot", null, String.class);
 
         // list data
-        String output = trt.getForObject("http://localhost:" + port + "/list", String.class);
+        String output = trt.getForObject("http://localhost:" + port + "/api/list", String.class);
 
         // Assertions:
         assertThat(output).contains("\"city\":\"Test1\",\"weather\":\"Rainy\"");
@@ -131,6 +123,6 @@ class SpringBootWeatherApplicationTests {
     }
 
     void deleteEntity(String id) {
-        trt.postForObject("http://localhost:" + port + "/delete?id=" + id, null, String.class);
+        trt.postForObject("http://localhost:" + port + "/api/delete?id=" + id, null, String.class);
     }
 }
