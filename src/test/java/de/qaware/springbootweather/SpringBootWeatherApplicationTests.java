29ebfbf5edb9c6b09adf9992c7b8479cf4ab1d5d
Robin Kalleicher <robin.kalleicherqaware.de>
2022-07-22 13:47:54 +0200

    implement a base integration test and simple tests for the application with new test container (postgresql)

--- src/test/java/de/qaware/springbootweather/SpringBootWeatherApplicationTests.java
+++ src/test/java/de/qaware/springbootweather/SpringBootWeatherApplicationTests.java
@@ -1,39 +1,47 @@
 package de.qaware.springbootweather;
 
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.test.system.OutputCaptureRule;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class SpringBootWeatherApplicationTests {
+class SpringBootWeatherApplicationTests extends BaseIntegrationTest {
 
-    @LocalServerPort
-    private int port;
-
-    @Autowired
-    private TestRestTemplate trt;
+    @Rule
+    public OutputCaptureRule output = new OutputCaptureRule();
 
     @Test
-    void contextLoads() {
+    void contextLoadsAndDatabaseIsOnline() {
+        assertThat(postgresDB.isRunning()).isTrue();
     }
 
     @Test
-    void shouldRetrieveWeatherRosenheim() {
-        String output = trt.getForObject("http://localhost:" + port + "/api/weather?city=Rosenheim", String.class);
+    void shouldRetrieveWeatherRosenheimFromOpenWeatherMap() {
+        ResponseEntity<String> response = trt.getForEntity("http://localhost:" + port + "/api/weather?city=Rosenheim", String.class);
 
         // Assertions:
-        assertThat(output).contains("The weather in 'Rosenheim' is currently: ");
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).contains("The weather in 'Rosenheim' is currently: ");
     }
 
     @Test
-    void shouldRetrieveWeatherMunich() {
-        String output = trt.getForObject("http://localhost:" + port + "/api/weather?city=Munich", String.class);
+    void shouldRetrieveWeatherMunichFromOpenWeatherMap() {
+        ResponseEntity<String> response = trt.getForEntity("http://localhost:" + port + "/api/weather?city=Munich", String.class);
 
         // Assertions:
-        assertThat(output).contains("The weather in 'Munich' is currently: ");
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).contains("The weather in 'Munich' is currently: ");
+    }
+
+    @Test
+    void shouldRetrieveWeatherForNoValidCity() {
+        ResponseEntity<String> response = trt.getForEntity("http://localhost:" + port + "/api/weather?city=Test", String.class);
+
+        // Assertions:
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).contains("No current weather data!");
     }
 }
